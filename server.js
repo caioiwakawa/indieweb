38f839655f7b8f631642b23079743825c53e619d
caio <caioiwakawa@gmail.com>
2026-05-05 20:17:36 -0300

    moved webpage to project

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
+const fs = require('fs').promises;
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get("/", (req, res) => {
+async function getLatestPost() {
+    const data = fs.readFile('posts.json')
+    const posts = JSON.parse(data)
+    const latest_post = posts.find(p => p.id)
+}
+
+app.get('/', (req, res) => {
     res.render('index')
 })
 
